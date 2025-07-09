25052b40bb0060387908a7db5c34abd132ebe120
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-07-09 14:23:55 +0530

    chore: removed connection boilerplate code in OrderDAOImpl

--- src/main/java/com/example/layeredarchitecture/dao/custom/impl/OrderDAOImpl.java
+++ src/main/java/com/example/layeredarchitecture/dao/custom/impl/OrderDAOImpl.java
@@ -1,6 +1,7 @@
 package com.example.layeredarchitecture.dao.custom.impl;
 
 import com.example.layeredarchitecture.dao.custom.OrderDAO;
+import com.example.layeredarchitecture.dao.custom.SQLUtil;
 import com.example.layeredarchitecture.db.DBConnection;
 import com.example.layeredarchitecture.model.ItemDTO;
 import com.example.layeredarchitecture.model.OrderDetailDTO;
@@ -16,29 +17,28 @@ public class OrderDAOImpl implements OrderDAO {
 
     @Override
     public String generateNewOrderId() throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        Statement stm = connection.createStatement();
-        ResultSet rst = stm.executeQuery("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
-
-        return rst.next() ? String.format("OID-%03d", (Integer.parseInt(rst.getString("oid").replace("OID-", "")) + 1)) : "OID-001";
+        String id = SQLUtil.execute("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
+        if (id != null) {
+            int newOrderId = Integer.parseInt(id.replace("O00-", "")) + 1;
+            return String.format("O00-%03d", newOrderId);
+        } else {
+            return "O00-001";
+        }
     }
 
     @Override
     public boolean existsOrder(String orderId) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement stm = connection.prepareStatement("SELECT oid FROM `Orders` WHERE oid=?");
-        stm.setString(1, orderId);
-        return stm.executeQuery().next();
+        return SQLUtil.execute("SELECT * FROM `Orders` WHERE oid=?", orderId) != null;
     }
 
     @Override
     public boolean saveOrder(String orderId, LocalDate orderDate, String customerId) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement stm = connection.prepareStatement("INSERT INTO `Orders` (oid, date, customerID) VALUES (?,?,?)");
-        stm.setString(1, orderId);
-        stm.setDate(2, Date.valueOf(orderDate));
-        stm.setString(3, customerId);
-        return stm.executeUpdate() > 0;
+        return SQLUtil.execute(
+                "INSERT INTO `Orders` (oid, date, customerID) VALUES (?,?,?)",
+                orderId,
+                orderDate,
+                customerId
+        );
     }
 
     @Override
@@ -88,11 +88,7 @@ public class OrderDAOImpl implements OrderDAO {
     @Override
     public ItemDTO findItem(String code) {
         try {
-            Connection connection = DBConnection.getDbConnection().getConnection();
-            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Item WHERE code=?");
-            pstm.setString(1, code);
-            ResultSet rst = pstm.executeQuery();
-            rst.next();
+            ResultSet rst = SQLUtil.execute("SELECT * FROM Item WHERE code=?", code);
             return new ItemDTO(code, rst.getString("description"), rst.getBigDecimal("unitPrice"), rst.getInt("qtyOnHand"));
         } catch (SQLException e) {
             throw new RuntimeException("Failed to find the Item " + code, e);
