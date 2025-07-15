8d16850836bb18acfee59f447cb9bd63beefe863
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-07-15 23:55:54 +0530

    feat: impl CrudDAO to order details

--- src/main/java/com/example/layeredarchitecture/dao/custom/OrderDetailDAO.java
+++ src/main/java/com/example/layeredarchitecture/dao/custom/OrderDetailDAO.java
@@ -1,11 +1,12 @@
 package com.example.layeredarchitecture.dao.custom;
 
+import com.example.layeredarchitecture.dao.CrudDAO;
 import com.example.layeredarchitecture.model.OrderDetailDTO;
 
 import java.sql.SQLException;
 import java.util.List;
 
-public interface OrderDetailDAO {
+public interface OrderDetailDAO extends CrudDAO<OrderDetailDTO> {
     public boolean saveOrderDetails(OrderDetailDTO orderDetailDTO) throws SQLException, ClassNotFoundException;
 
     public List<OrderDetailDTO> getOrderDetails(String orderId) throws SQLException, ClassNotFoundException;
