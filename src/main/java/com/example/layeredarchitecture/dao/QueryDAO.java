ddb762b025d563057dc36778dc31bcd5a37e6d39
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-07-23 12:30:52 +0530

    refactor: applied entity for every QueryDAO

--- src/main/java/com/example/layeredarchitecture/dao/QueryDAO.java
+++ src/main/java/com/example/layeredarchitecture/dao/QueryDAO.java
@@ -1,11 +1,12 @@
 package com.example.layeredarchitecture.dao;
 
 import com.example.layeredarchitecture.dto.CustomDTO;
+import com.example.layeredarchitecture.entity.Custom;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface QueryDAO extends SuperDAO {
-    List<CustomDTO> getAllCustomersByOrder() throws SQLException, ClassNotFoundException;
-    List<CustomDTO> getAllCustomersByItem() throws SQLException, ClassNotFoundException;
+    List<Custom> getAllCustomersByOrder() throws SQLException, ClassNotFoundException;
+    List<Custom> getAllCustomersByItem() throws SQLException, ClassNotFoundException;
 }
--- src/main/java/com/example/layeredarchitecture/dao/custom/impl/QueryDAOImpl.java
+++ src/main/java/com/example/layeredarchitecture/dao/custom/impl/QueryDAOImpl.java
@@ -2,18 +2,19 @@ package com.example.layeredarchitecture.dao.custom.impl;
 
 import com.example.layeredarchitecture.dao.QueryDAO;
 import com.example.layeredarchitecture.dto.CustomDTO;
+import com.example.layeredarchitecture.entity.Custom;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class QueryDAOImpl implements QueryDAO {
     @Override
-    public List<CustomDTO> getAllCustomersByOrder() throws SQLException, ClassNotFoundException {
+    public List<Custom> getAllCustomersByOrder() throws SQLException, ClassNotFoundException {
         return null;
     }
 
     @Override
-    public List<CustomDTO> getAllCustomersByItem() throws SQLException, ClassNotFoundException {
+    public List<Custom> getAllCustomersByItem() throws SQLException, ClassNotFoundException {
         return List.of();
     }
 }
